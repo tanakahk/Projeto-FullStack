2509c1f5dd30827809e47bc17bab63d52a820518
Tanaka <74397585+tanakahk@users.noreply.github.com>
2021-07-10 02:23:24 -0300

    melhorias frontend
    1. migrações de código para o módulo.
    2. loading de imagem agora monta ela com as 3 camadas
    3. criação do comp frame para manipular a skill
    4. criação do skillConstructor para renderizar as skills em camadas
    5. algumas imagens adicionadas

---
+++ src/components/Frame.vue
@@ -0,0 +1,67 @@
+<template >
+  <div class="storeAlignContainer">
+    <div class="centralizeItens">
+      <div @click="modalStatusOpen(url)" v-for="url in frame.state.srUrl" :key="url">
+        <skill-constructor :url="url" />
+      </div>
+    </div>
+  </div>
+  <div>
+    <button class="buttoon" @click="frame.actions.loadSr(15, false)">
+      <strong>Load Skill Record</strong>
+    </button>
+  </div>
+</template>
+
+<script lang="ts">
+import useFrame from '@/modules/frame';
+import { defineComponent } from 'vue';
+import SkillConstructor from './SkillConstructor.vue';
+
+export default defineComponent({
+  components: { SkillConstructor },
+  setup() {
+    const frame = useFrame();
+
+    frame.actions.loadSr(15, true);
+
+    const modalStatusOpen = (url: string) => {
+      const novoUrl = url.slice(91, 98);
+      console.log('modal aberto', novoUrl);
+    };
+
+    return {
+      frame,
+      modalStatusOpen,
+    };
+  },
+});
+</script>
+
+<style scoped>
+
+.storeAlignContainer {
+  justify-content: center;
+  display: flex;
+  flex-flow: row;
+  flex-wrap: wrap;
+  width: 100%;
+}
+
+.centralizeItens {
+  display: flex;
+  flex-flow: row;
+  flex-wrap: wrap;
+  max-width: 1500px;
+}
+
+.buttoon {
+  margin-top: 50px;
+  font-size: 35px;
+  border-radius: 10px;
+  background-color: rgb(56, 123, 199);
+  color: white;
+  padding: 10px;
+}
+
+</style>
--- src/modules/frame.ts
+++ src/modules/frame.ts
@@ -1,28 +1,18 @@
 // import axios from 'axios';
 import { reactive, readonly } from 'vue';
 
-// interface UseTemplate {
-//   state: State;
-//   mutations: Mutations;
-//   actions: Actions;
-// }
-
-export interface Sr {
-  url: string
-}
-
-export interface State {
-  sr: Sr[]
+export interface FrameState {
   busy: boolean
+  srUrl: string[]
+  srCurrent: number
+  defaultSrUrl: string
 }
 
-// interface Mutations {}
-
-// interface Actions {}
-
-const state: State = reactive({
-  sr: [],
+const state: FrameState = reactive({
   busy: false,
+  srUrl: [],
+  srCurrent: 0,
+  defaultSrUrl: 'https://github.com/Nayuta-Kani/SAOIF-Skill-Records-Database/blob/master/srimages/sr_icon_l_6100',
 });
 
 const mutations = {
@@ -33,15 +23,61 @@ const mutations = {
     } else {
       console.log('Estou disponível');
     }
+    return true;
   },
 
+  setSrUrl(res: string) {
+    state.srUrl.push(res);
+    return true;
+  },
+
+  // setSrId(id: string) {
+
+  // }
 };
 
 const actions = {
-  async loadSr() {
+  async loadSr(count: number, firstTime: boolean) {
     mutations.setBusy(true);
 
+    if (firstTime) {
+      // eslint-disable-next-line no-param-reassign
+      count++;
+    }
+
+    for (let i = 1; i <= count; i++) {
+      let srId = '';
+      let res = '';
+      if (state.srCurrent < 10) {
+        srId = srId.concat('00', state.srCurrent.toString());
+      }
+      if (state.srCurrent >= 10 && state.srCurrent < 100) {
+        srId = srId.concat('0', state.srCurrent.toString());
+      }
+      if (state.srCurrent >= 100) {
+        srId = state.srCurrent.toString();
+      }
+
+      res = res.concat(`${state.defaultSrUrl}`, srId, '.png?raw=true');
+      mutations.setSrUrl(res);
+      state.srCurrent++;
+    }
+
+    if (firstTime) {
+      state.srUrl.shift();
+    }
+
+    // ------------- Para a gente testar os valores ------------- //
+
+    // for (let i = 0; i < state.srUrl.length; i++) {
+    //   console.log('srUrl', i, state.srUrl[i]);
+    // }
+    // for (let i = 0; i < state.srCurrent; i++) {
+    //   console.log('state.srCurrent', state.srCurrent);
+    // }
+
     mutations.setBusy(false);
+    return true;
   },
 };
 
